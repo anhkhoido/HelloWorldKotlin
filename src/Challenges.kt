3d820b6600a0b692156a63c1d51eed43f124163f
anhkhoido <anhkhoido@gmail.com>
2020-05-31 16:29:14 -0400

    Added a challenge about removing duplicates from a List<T>

--- src/Challenges.kt
+++ src/Challenges.kt
@@ -3,4 +3,17 @@ fun main() {
     // Challenge 1: How to filter a sequence and convert it to a List<T> in Kotlin.
     val result = (0 until 100).asSequence().map { it }.filter { it % 10 == 0 }.toList()
     println(result)
+
+    println()
+
+    // Challenge 2: Remove duplicates from a List<T> of films I recommend!
+    val greatFilms = listOf<String>("Crouching Tiger, Hidden Dragon", "The Farewell", "Where the Truth Lies", "Pontypool", "The Sweet Hereafter", "Pontypool", "Maps to the Stars", "In the Mood for Love", "2046", "The Sweet Hereafter", "Get Out", "Get Out", "Sicario")
+    removeDuplicatesFromCollection(greatFilms)
+}
+
+fun removeDuplicatesFromCollection(list: Collection<String>) {
+    val removingDuplicates = list.toSet()
+    for (film in removingDuplicates) {
+        println(film)
+    }
 }
