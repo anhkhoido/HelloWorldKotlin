57c020ec6725f9d37a47d8d02c7e2b12e47b84a2
anhkhoido <anhkhoido@gmail.com>
2020-06-06 07:00:23 -0400

    Created a demo of lambda functions.

--- src/functions.kt
+++ src/functions.kt
@@ -6,7 +6,15 @@ fun main() {
 
     // Calculate your expected net worth.
     println("Your expected net worth is ${calculateExpectedNetWorth(25, 40000)}\$.")
+
+    println()
+    passFunctionToFilter()
 }
 
 fun calculateETA(distance: Double, speed: Double) : Double = distance / speed
 fun calculateExpectedNetWorth(age : Int, income : Int) = (age * income) / 10
+
+fun passFunctionToFilter() {
+    val myList = listOf<String>("Anh Khoi Do", "Patrick Senécal", "Firefox", "Sophie", "Inoue", "Eileen")
+    println(myList.filter(fun(word: String) = !word.contains('a', true)))
+}
